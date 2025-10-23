36952c3e7f31ba55b687b921ccfcaa057b9c6d1d
Jorjais5619 <a24310387@live.ceti.mx>
2025-10-23 02:36:45 +0000

    practica 4

---
+++ src/ejemplo.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <thread>
+#include <chrono>
+#include <ftxui/screen/screen.hpp>
+
+using namespace std;
+using namespace ftxui;
+
+int main(int argc, char const *argv[])
+{
+    Screen pantalla=Screen::Create(
+        Dimension::Full(),
+        Dimension::Fixed (10)
+    );
+   int x=0;
+   int y=0;
+   while(true){
+        Pixel& pixel=pantalla.PixelAt(x,y);
+        pixel.foreground_color=Color::Red;
+        pixel.background_color=Color::Green;
+        pixel.character='X';
+        
+        pantalla.Print();
+        this_thread::sleep_for(chrono::seconds(1));
+        pantalla.Clear();
+        cout<<pantalla.ResetPosition();
+        x++;
+   }
+
+    return 0;
+}
